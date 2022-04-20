75a6026815c42d0b1db3b720494c2d52d3379bee
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-19 20:47:46 -0600

    Create TicketType.cs

---
+++ Models/TicketType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Type Name")]
+        public string Name { get; set; }
+    }
+}
