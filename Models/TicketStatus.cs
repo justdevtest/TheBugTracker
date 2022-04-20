911414633d399e4cf822a9e063fe610929717e0e
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-19 20:49:13 -0600

    Create TicketStatus.cs

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Status Name")]
+        public string Status { get; set; }
+    }
+}
