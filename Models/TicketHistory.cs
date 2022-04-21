8ea458875d249ae9e3ed2dae77a7517cddae5885
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-20 21:24:19 -0600

    Create TicketHistory.cs

---
+++ Models/TicketHistory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Ticket")]
+        public int TicketId { get; set; }
+        
+        [Display(Name = "Updated Item")]
+        public string Property { get; set; }
+
+        [Display(Name = "Previous")]
+        public string OldValue { get; set; }
+
+        [Display(Name = "Current")]
+        public string NewValue { get; set; }
+
+        [Display(Name = "Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+        [Display(Name = "Description of Change")]
+        public string Description { get; set; }
+
+        [Display(Name = "Team Member")]
+        public string UserId { get; set; }
+
+        // Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
