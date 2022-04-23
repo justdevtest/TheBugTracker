5abed54f417f79145552444d3df35e673fc49e3b
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-23 08:07:33 -0600

    Update TicketComment.cs
    Added data annotations

--- Models/TicketComment.cs
+++ Models/TicketComment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace TheBugTracker.Models
     public class TicketComment
     {
         public int Id { get; set; }
+
+        [Display(Name = "Ticket")]
         public int TicketId { get; set; }
+
+        [Display(Name = "Team Member")]
         public string UserId { get; set; }
+
+        [Display(Name = "Member Comment")]
         public string Comment { get; set; }
+
+        [Display(Name = "Date")]
         public DateTimeOffset Created { get; set; }
 
         // Navigation Properties
