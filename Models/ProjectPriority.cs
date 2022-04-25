6e3b0f351aff739abbaa8cab91bfb87e436071e8
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-24 20:21:17 -0600

    Create ProjectPriority.cs

---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Project Name")]
+        public string Name { get; set; }
+    }
+}
