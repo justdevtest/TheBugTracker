f76dcb025c685c2c166c5d4dc575f07550242631
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-28 20:35:16 -0600

    Create Company.cs

---
+++ Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class Company
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Company Description")]
+        public string Description { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+
+    }
+}
