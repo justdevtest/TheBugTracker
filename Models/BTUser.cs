52271258911da894740e2a51c62fe3289a1e72e5
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-04-28 20:47:27 -0600

    Update BTUser.cs

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -22,6 +23,23 @@ namespace TheBugTracker.Models
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
 
+        [Display(Name = "Company Id")]
+        public int? CompanyId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+        
+        [Display(Name = "Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+
+        // Navigation Property
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
 
 
     }
