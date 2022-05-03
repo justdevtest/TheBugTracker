8af11f04c28ef06f0ce7553b7bacd7d3fd69b2a8
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-05-02 21:09:39 -0600

    Create Invite.cs

---
+++ Models/Invite.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class Invite
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        [Display(Name = "Date Sent")]
+        public DateTimeOffset InviteDate { get; set; }
+
+        [Display(Name = "Date Joined")]
+        public DateTimeOffset JoinDate { get; set; }
+
+        [Display(Name = "Code")]
+        public Guid CompanyToken { get; set; }
+
+        [Display(Name = "Company")]
+        public int CompanyId { get; set; }
+
+        [Display(Name = "Project")]
+        public int ProjectId { get; set; }
+
+        [Display(Name = "Invitor")]
+        public string InvitorId { get; set; }
+
+        [Display(Name = "Invitee")]
+        public string InviteeId { get; set; }
+
+        [Display(Name = "Invitee Email")]
+        public string InviteeEmail { get; set; }
+
+        [Display(Name = "Invitee First Name")]
+        public string InviteeFirstName { get; set; }
+
+        [Display(Name = "Invitee Last Name")]
+        public string InviteeLastName { get; set; }
+
+        // May be referenced as a "flag" (i.e. "Create a flag in the database)
+        public bool IsValid { get; set; }
+
+
+        // Navigation Properties
+        public virtual Company Company { get; set; }
+        public virtual Project Project { get; set; }
+        public virtual BTUser Invitor { get; set; }
+        public virtual BTUser Invitee { get; set; }
+
+
+    }
+}
