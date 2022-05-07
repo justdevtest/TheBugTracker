48992ae5c7c35f258abdea69b72c06d784bb1ccc
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-05-07 10:54:51 -0600

    Update BTRolesService.cs

--- Services/BTRolesService.cs
+++ Services/BTRolesService.cs
@@ -43,14 +43,19 @@ namespace TheBugTracker.Services
             return result;
         }
 
-        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        public async Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
         {
-            throw new NotImplementedException();
+            List<BTUser> users = (await _userManager.GetUsersInRoleAsync(roleName)).ToList();
+            List<BTUser> result = users.Where(u => u.CompanyId == companyId).ToList();
+            return result;
         }
 
-        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        public async Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
         {
-            throw new NotImplementedException();
+            List<string> userIds = (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
+            List<BTUser> roleUsers = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+            List<BTUser> result = roleUsers.Where(u => u.CompanyId == companyId).ToList();
+            return result;
         }
 
         public Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
