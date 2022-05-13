570c526b477f49a44fddc994e80013ed14814dab
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-05-13 15:10:18 -0600

    CompanyInfoService Implementation

--- Models/Company.cs
+++ Models/Company.cs
@@ -20,6 +20,7 @@ namespace TheBugTracker.Models
         // Navigation Properties
         public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
         public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+        public virtual ICollection<Invite> Invites { get; set; } = new HashSet<Invite>();
 
     }
 }
