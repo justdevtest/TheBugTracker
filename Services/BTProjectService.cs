2bf211bf1d3fbad5d5f3589ca7f9d20e613cda10
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-05-22 09:05:41 -0600

    Update BTProjectService.cs
    GetUserProjects

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -148,9 +148,41 @@ namespace TheBugTracker.Services
             throw new NotImplementedException();
         }
 
-        public Task<List<Project>> GetUserProjectsAsync(string userId)
+        public async Task<List<Project>> GetUserProjectsAsync(string userId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Project> userProjects = (await _context.Users
+                    .Include(u => u.Projects)
+                        .ThenInclude(p => p.Company)
+                    .Include(u => u.Projects)
+                        .ThenInclude(p => p.Members)
+                    .Include(u => u.Projects)
+                        .ThenInclude(p => p.Tickets)
+                    .Include(u => u.Projects)
+                        .ThenInclude(t => t.Tickets)
+                            .ThenInclude(t => t.DeveloperUser)
+                    .Include(u => u.Projects)
+                        .ThenInclude(t => t.Tickets)
+                            .ThenInclude(t => t.OwnerUser)
+                    .Include(u => u.Projects)
+                        .ThenInclude(t => t.Tickets)
+                            .ThenInclude(t => t.TicketPriority)
+                    .Include(u => u.Projects)
+                        .ThenInclude(t => t.Tickets)
+                            .ThenInclude(t => t.TicketStatus)
+                    .Include(u => u.Projects)
+                        .ThenInclude(t => t.Tickets)
+                            .ThenInclude(t => t.TicketType)
+                    .FirstOrDefaultAsync(u => u.Id == userId)).Projects.ToList();
+                
+                return userProjects;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***** Error ***** - Error Getting User Project List ---> {ex.Message}");
+                throw;
+            }
         }
 
         public Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
@@ -207,6 +239,7 @@ namespace TheBugTracker.Services
             catch (Exception ex)
             {
                 Console.WriteLine($"***** Error ***** - Error Removing User from Project ---> {ex.Message}");
+                throw;
             }
         }
 
