c7752f4af867efa4ee9b26f74207a241d6b69d09
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-06-16 21:46:13 -0600

    Create BTTicketHistoryService.cs
    Started AddHistoryAsync If statement

---
+++ Services/BTTicketHistoryService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTTicketHistoryService : IBTTicketHistoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTTicketHistoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            // New Ticket has been added
+            if (oldTicket == null && newTicket != null)
+            {
+                // Instantiate and access the Ticket History properties "in-line"
+                TicketHistory history = new()
+                {
+                    TicketId = newTicket.Id,
+                    Property = "",
+                    OldValue = "",
+                    NewValue = "",
+                    Created = DateTimeOffset.Now,
+                    UserId = userId,
+                    Description = "New Ticket Created"
+                };
+
+                try
+                {
+                    await _context.TicketHistories.AddAsync(history);
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
