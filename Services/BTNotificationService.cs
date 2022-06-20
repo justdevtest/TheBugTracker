bd919923a75385d422fd876d57ae160c0650ace6
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-06-20 09:21:56 -0600

    BTNoticificationService
    Interface + Implementing Class

---
+++ Services/BTNotificationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTNotificationService : IBTNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetReceivedNotificationAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetSentNotificationAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotification(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
