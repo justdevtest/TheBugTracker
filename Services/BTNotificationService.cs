d0114445d80c97905a99fbc433e46b43162b1c4e
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-06-20 09:27:36 -0600

    Update BTNotificationService.cs
    AddNotificationAsync

--- Services/BTNotificationService.cs
+++ Services/BTNotificationService.cs
@@ -22,9 +22,18 @@ namespace TheBugTracker.Services
             _roleService = roleService;
         }
 
-        public Task AddNotificationAsync(Notification notification)
+        public async Task AddNotificationAsync(Notification notification)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }            
         }
 
         public Task<List<Notification>> GetReceivedNotificationAsync(string userId)
