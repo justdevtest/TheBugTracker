e66e8d99cb5a4e970325ab6b1a41fa4cebf77768
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-06-23 16:10:02 -0600

    BTInvite service and interface

---
+++ Services/BTInviteService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTInviteService : IBTInviteService
+    {
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
