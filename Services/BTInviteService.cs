32c4827e618dd1a11aad3595bc42a85f0bacf821
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-06-28 21:39:14 -0600

    Update BTInviteService.cs
    ValidateInviteCodeAsync

--- Services/BTInviteService.cs
+++ Services/BTInviteService.cs
@@ -89,14 +89,49 @@ namespace TheBugTracker.Services
             }
         }
 
-        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        public async Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Invite invite = await _context.Invites.Where(i => i.CompanyId == companyId)
+                                                      .Include(i => i.Company)
+                                                      .Include(i => i.Project)
+                                                      .Include(i => i.Invitor)
+                                                      .FirstOrDefaultAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+                return invite;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
-        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        public async Task<bool> ValidateInviteCodeAsync(Guid? token)
         {
-            throw new NotImplementedException();
+            if (token == null)
+            {
+                return false;
+            }
+
+            bool result = false;
+            Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+            if (invite != null)
+            {
+                DateTime inviteDate = invite.InviteDate.DateTime;
+
+                // Custom validation of invite based on date it was issued
+                // We are allowing an invite to be valid for 7 days
+                bool validDate = (DateTime.Now - inviteDate).TotalDays <= 7;
+
+                if (validDate)
+                {
+                    result = invite.IsValid;
+                }
+            }
+            return result;
+
         }
     }
 }
