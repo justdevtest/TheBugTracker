c304abd4bb4247bf14dfcb731755be83c1562d6a
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-07-03 09:07:18 -0600

    ConvertByteArrayToFile

--- Services/BTFileService.cs
+++ Services/BTFileService.cs
@@ -9,9 +9,20 @@ namespace TheBugTracker.Services
 {
     public class BTFileService : IBTFileService
     {
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
         public string ConvertByteArrayToFile(byte[] fileData, string extension)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData);
+                return string.Format($"data:{extension};base64,{imageBase64Data}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
