06cffbf511bf870a3ec40b0f5906276360126e62
jcodesthings <55113474+halfcaffdev@users.noreply.github.com>
2022-07-18 21:25:39 -0600

    Add GetRolesAsync

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -3,13 +3,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Extensions;
+using TheBugTracker.Models;
+using TheBugTracker.Models.ViewModels;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Controllers
 {
     public class UserRolesController : Controller
     {
-        public IActionResult Index()
+        private readonly IBTRolesService _rolesService;
+        private readonly IBTCompanyInfoService _companyInfoService;
+
+        public UserRolesController(IBTRolesService rolesService, IBTCompanyInfoService companyInfoService)
         {
+            _rolesService = rolesService;
+            _companyInfoService = companyInfoService;
+        }
+
+        public async Task<IActionResult> ManageUserRoles()
+        {
+            // Add instance of the ViewModel as a List (model)
+            List<ManageUserRolesViewModel> model = new();
+
+            // Get CompanyId
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            // Get all Company users
+            List<BTUser> users = await _companyInfoService.GetAllMembersAsync(companyId);
+
+            // Loop over the users to populate the ViewModel
+
+            // - use _rolesService
+            // - create multi-select list
+            foreach(BTUser user in users)
+            {
+                // - instantiate ViewModel
+                ManageUserRolesViewModel viewModel = new();
+            }
+            
+            // Return model to the view
             return View();
         }
     }
