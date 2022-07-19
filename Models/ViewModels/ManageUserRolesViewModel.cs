763af12ede5c7d6be265de1a2886d0dace8abf13
jcodesthings <55113474+halfcaffdev@users.noreply.github.com>
2022-07-18 20:47:15 -0600

    Update ManageUserRolesViewModel.cs

--- Models/ViewModels/ManageUserRolesViewModel.cs
+++ Models/ViewModels/ManageUserRolesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace TheBugTracker.Models.ViewModels
 {
     public class ManageUserRolesViewModel
     {
-
+        // Responsible for moving data within the MVC structure for this feature
+        public BTUser BTUser { get; set; }
+        public MultiSelectList Roles { get; set; }
+        public List<String> SelectedRoles { get; set; }
     }
 }
